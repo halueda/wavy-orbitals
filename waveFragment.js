ea05d80f3bc291277bc66c19cbfa0855fc5a224a
Haruyasu Ueda <ueda.haruyasu@fujitsu-general.com>
2022-11-05 21:52:50 +0900

    new: waveFragment.js and sampled points are denoted as WaveFragment object. changed: filename of cyclic_colorscale.js to cyclicColorscale.js for convention.

---
+++ waveFragment.js
@@ -0,0 +1,13 @@
+
+export default class WaveFragment {
+    constructor(x, y, z, init_theta, angular_velocity, parent=null, visible=true, ){
+	this.x = x;
+	this.y = y;
+	this.z = z;
+	this.init_theta = init_theta;
+	this.angular_velocity = angular_velocity;
+	this.parent = parent;
+	this.visible = visible;
+    }
+	
+}
