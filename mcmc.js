d5ca79744bd8cb4775477b049eec78102d0233c2
Haruyasu Ueda <hal.ueda.1616@gmail.com>
2022-11-10 12:55:28 +0900

    Update mcmc.js
    comment out Vue related code

--- mcmc.js
+++ mcmc.js
@@ -1,47 +1,47 @@
 // borrowed from https://github.com/todays-mitsui/mhatl.js/blob/master/src/App.vue
-import { Component, Vue } from 'vue-property-decorator'
-import ControlPanel from './components/ControlPanel.vue'
-import DisplayScatter from './components/display/DisplayScatter.vue'
-import DisplayHistgram from './components/display/DisplayHistgram.vue'
-import DisplayTraceline from './components/display/DisplayTraceline.vue'
-import { Point, Sample } from '../lib/interfaces'
-import rnorm from '../lib/util/rnorm'
-import uniform from '../lib/util/uniform'
+// import { Component, Vue } from 'vue-property-decorator'
+// import ControlPanel from './components/ControlPanel.vue'
+// import DisplayScatter from './components/display/DisplayScatter.vue'
+// import DisplayHistgram from './components/display/DisplayHistgram.vue'
+// import DisplayTraceline from './components/display/DisplayTraceline.vue'
+// import { Point, Sample } from '../lib/interfaces'
+import rnorm from './rnorm'
+import uniform from './uniform'
 /** 提案分布の標準偏差 */
 const SIGMA = 1.0
 /** 目標分布(二変量正規分布)の共分散 */
 const b = 0.5
 /** Burn-in 期間の長さ */
 const BURN_IN_PERIOD = 50
-@Component({
-  components: {
-    ControlPanel,
-    DisplayScatter,
-    DisplayHistgram,
-    DisplayTraceline
-  }
-})
-export default class App extends Vue {
-  /** サンプリングの初期値 x軸の値 */
-  private x0 = 3.0
-  /** サンプリングの初期値 y軸の値 */
-  private y0 = 3.0
-  /** 生成したサンプル数 (Burn-in 期間や棄却されたものを含む) */
-  private count = 0
-  /** 生成したサンプル列 (Burn-in 期間や棄却されたものを含む) */
-  private samples: Sample[] = []
-  // ======================================================================== //
-  /** 目標分布のカーネル */
-  p ({ x, y }: Point): number {
-    return Math.exp(-0.5 * (x * x - 2 * b * x * y + y * y))
-  }
-  /** 提案分布 - 正規分布からの実現値の組 */
-  q ({ x, y }: Point): Point {
-    return {
-      x: x + rnorm(0, SIGMA),
-      y: y + rnorm(0, SIGMA)
-    }
-  }
+// @Component({
+//   components: {
+//     ControlPanel,
+//     DisplayScatter,
+//     DisplayHistgram,
+//     DisplayTraceline
+//   }
+// })
+// export default class App extends Vue {
+//   /** サンプリングの初期値 x軸の値 */
+   private x0 = 3.0
+//   /** サンプリングの初期値 y軸の値 */
+   private y0 = 3.0
+//   /** 生成したサンプル数 (Burn-in 期間や棄却されたものを含む) */
+   private count = 0
+//   /** 生成したサンプル列 (Burn-in 期間や棄却されたものを含む) */
+   private samples: Sample[] = []
+//   // ======================================================================== //
+//   /** 目標分布のカーネル */
+   p ({ x, y }: Point): number {
+     return Math.exp(-0.5 * (x * x - 2 * b * x * y + y * y))
+   }
+//   /** 提案分布 - 正規分布からの実現値の組 */
+   q ({ x, y }: Point): Point {
+     return {
+       x: x + rnorm(0, SIGMA),
+       y: y + rnorm(0, SIGMA)
+     }
+   }
   /** サンプル取得処理 */
   sample (): Sample {
     // 点 current 、初回は current === undefined なので { x0, y0 } を使う
@@ -90,15 +90,15 @@ export default class App extends Vue {
       y: this.y0
     }
   }
-  onChangePause () {
-    if (!this.pause) { this.loop() }
-  }
-  onReset () {
-    this.pause = true
-    setTimeout(() => {
-      this.count = 0
-      this.current = undefined
-      this.samples = []
-    }, 1000 / this.speed)
-  }
+//   onChangePause () {
+//     if (!this.pause) { this.loop() }
+//   }
+//   onReset () {
+//     this.pause = true
+//     setTimeout(() => {
+//       this.count = 0
+//       this.current = undefined
+//       this.samples = []
+//     }, 1000 / this.speed)
+//   }
 }
