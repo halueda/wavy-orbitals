a27496ea078cd6fe6a59b18b390fc951509fb860
Ueda Haruyasu <ueda.haruyasu@fujitsu-general.com>
2022-11-14 19:49:01 +0900

    WIP

--- mcmcSampler.js
+++ mcmcSampler.js
@@ -1,18 +1,12 @@
-// borrowed from https://github.com/todays-mitsui/mhatl.js/blob/master/src/App.vue
+// From https://github.com/todays-mitsui/mhatl.js/blob/master/src/App.vue
 // import { Component, Vue } from 'vue-property-decorator'
 // import ControlPanel from './components/ControlPanel.vue'
 // import DisplayScatter from './components/display/DisplayScatter.vue'
 // import DisplayHistgram from './components/display/DisplayHistgram.vue'
 // import DisplayTraceline from './components/display/DisplayTraceline.vue'
 // import { Point, Sample } from '../lib/interfaces'
-import rnorm from './rnorm'
-import uniform from './uniform'
-/** 提案分布の標準偏差 */
-const SIGMA = 1.0
-/** 目標分布(二変量正規分布)の共分散 */
-const b = 0.5
-/** Burn-in 期間の長さ */
-const BURN_IN_PERIOD = 50
+//import rnorm from './rnorm'
+//import uniform from './uniform'
 // @Component({
 //   components: {
 //     ControlPanel,
@@ -21,85 +15,109 @@ const BURN_IN_PERIOD = 50
 //     DisplayTraceline
 //   }
 // })
+// From https://github.com/todays-mitsui/mhatl.js/blob/master/lib/util/rnorm.ts
+/**
+ * 正規分布から乱数を生成
+ *
+ * @param m 平均
+ * @param s 標準偏差
+ */
+function rnorm (m = 0, s = 1) {
+  const rnorm = Math.sqrt(-2 * Math.log(1 - Math.random())) * Math.cos(2 * Math.PI * Math.random())
+  return (rnorm + m) * s * s
+}
+
+// From https://github.com/todays-mitsui/mhatl.js/blob/master/lib/util/uniform.ts
+/**
+ * 一様分布から乱数を生成
+ *
+ * @param min
+ * @param max
+ */
+function uniform (min: number, max: number) {
+  return Math.random() * (max - min) + min
+}
+
+
+// From https://github.com/todays-mitsui/mhatl.js/blob/master/src/App.vue
+
 // export default class App extends Vue {
 export default class MCMCSampler {
-  /** サンプリングの初期値 x軸の値 */
-  private x0 = 3.0
-  /** サンプリングの初期値 y軸の値 */
-  private y0 = 3.0
-  /** 生成したサンプル数 (Burn-in 期間や棄却されたものを含む) */
-  private count = 0
-  /** 生成したサンプル列 (Burn-in 期間や棄却されたものを含む) */
-  private samples: Sample[] = []
-  // ======================================================================== //
-  /** 目標分布のカーネル */
-   p ({ x, y }: Point): number {
-     return Math.exp(-0.5 * (x * x - 2 * b * x * y + y * y))
-   }
-  /** 提案分布 - 正規分布からの実現値の組 */
-   q ({ x, y }: Point): Point {
-     return {
-       x: x + rnorm(0, SIGMA),
-       y: y + rnorm(0, SIGMA)
-     }
-   }
-  /** サンプル取得処理 */
-  sample (): Sample {
-    // 点 current 、初回は current === undefined なので { x0, y0 } を使う
-    const current = this.current || { x: this.x0, y: this.y0 }
-    const next = this.q(current)      // 提案分布を使って点 next を選ぶ
-    const pCurrent = this.p(current)  // 点 current における目標分布の確率密度
-    const pNext = this.p(next)        // 点 next における目標分布の確率密度
-    const r = pNext / pCurrent        // 受容確率
-    // r が 1 以上ならそのまま受容する (accepted === true になる)
-    // r が 1 より小さいなら、確率 r で受容する
-    const accepted = r >= 1 || r > uniform(0, 1)
-//     console.table({
-//       current: `(${current.x.toFixed(2)}, ${current.y.toFixed(2)})`,
-//       next: `(${next.x.toFixed(2)}, ${next.y.toFixed(2)})`,
-//       pCurrent: pCurrent.toFixed(2),
-//       pNext: pNext.toFixed(2),
-//       r: r.toFixed(2),
-//       result: accepted ? 'accepted' : 'rejected'
-//     })
-    return {
-      count: this.count,
-      burnin: this.count < BURN_IN_PERIOD,
-      current,
-      next,
-      result: accepted ? 'accepted' : 'rejected'
+    private count = 0
+    // ======================================================================== //
+//    /** 目標分布のカーネル */
+//	/** 目標分布(二変量正規分布)の共分散 */
+//	b=0.5,
+//    p ({ x, y }: Point): number {
+//	return Math.exp(-0.5 * (x * x - 2 * b * x * y + y * y))
+//    }
+    /** 提案分布 - 正規分布からの実現値の組 */
+    q ({ x, y, z } /*: Point*/ ) /*: Point*/ {
+	return {
+	    x: rnorm(x, this.SIGMA),
+	    y: rnorm(y, this.SIGMA),
+	    z: rnorm(z, this.SIGMA)
+	}
     }
-  }
-  /** サンプル取得を繰り返す実行するためのループ */
-  loop () {
-    // サンプル取得
-    const sample = this.sample()
-    // 取得した sample が受容されていた場合は、次に備えて点 current を点 next で置き換える
-    if (sample.result === 'accepted') { this.current = sample.next }
-    this.samples.push(sample)
-    this.count += 1
-    // 1000 / speed ミリ秒だけ待ってから loop() を再び呼び出す
-    if (!this.pause) { setTimeout(this.loop, 1000 / this.speed) }
-  }
-  // ======================================================================== //
-  private pause = true
-  private speed = 8
-  private current?: Point
-  get p0 (): Point {
-    return {
-      x: this.x0,
-      y: this.y0
+    constructor(
+	/** 目標分布のカーネル */
+	p,
+	x0=0, y0=0, z0=0,
+	/** Burn-in 期間の長さ */
+	burn_in_period=50,
+	/** 提案分布の標準偏差 */
+	sigma=1.0,
+    ){
+	this.p = p;
+	this.current = {x: x0, y: y0, z: z0};
+	this.BURN_IN_PERIOD = burn_in_period;
+	this.SIGMA = sigma;
+	burnIn();
     }
-  }
-//   onChangePause () {
-//     if (!this.pause) { this.loop() }
-//   }
-//   onReset () {
-//     this.pause = true
-//     setTimeout(() => {
-//       this.count = 0
-//       this.current = undefined
-//       this.samples = []
-//     }, 1000 / this.speed)
-//   }
+
+    /** サンプル取得処理 */
+    sample0 () {
+	// 点 current 、初回は current === undefined なので { x0, y0 } を使う
+	// const current = this.current || { x: this.x0, y: this.y0 }
+	const next = this.q(current)      // 提案分布を使って点 next を選ぶ
+	const pCurrent = this.p(current)  // 点 current における目標分布の確率密度
+	const pNext = this.p(next)        // 点 next における目標分布の確率密度
+	const r = pNext / pCurrent        // 受容確率
+	// r が 1 以上ならそのまま受容する (accepted === true になる)
+	// r が 1 より小さいなら、確率 r で受容する
+	const accepted = r >= 1 || r > uniform(0, 1)
+	this.count +=1;
+	return {
+	    current,
+	    next,
+	    result: accepted,
+	}
+    }
+
+    /** サンプル取得して更新 */
+    sample1 (): {
+	a_sample = sample0();
+	// 取得した sample が受容されていた場合は、次に備えて点 current を点 next で置き換える
+	if (a_sample.result) {
+	    this.current = sample.next 
+	    return sample.next;
+	} else {
+	    return false;
+	}
+    }
+
+    burnIn() {
+	while (this.count < this.BURN_IN_PERIOD) {
+	    sample1();
+	}
+    }
+
+    /** acceptされたサンプルを返す */
+    sample (): {
+	a_sample = sample1();
+	while ( a_sample == false) {
+	    a_sample = sample1();
+	}
+	return a_sample;
+    }
 }
