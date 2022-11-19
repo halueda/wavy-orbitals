3c365ddef84773bb6c604dd088abad7d27184452
Haruyasu Ueda <ueda.haruyasu@fujitsu-general.com>
2022-11-19 09:19:43 +0900

    WIP use MCMCSampler for sample method

--- waveFunction.js
+++ waveFunction.js
@@ -3,19 +3,21 @@ export default class WaveFunction {
     /* これは一番抽象的なクラス。ユーティリティ関数を含む */
     /* 引数の point は {x: num, y: num, z: num} 形式。移動のライブラリを使いたいから */
     /* 結果の複素数は */
+    init() {
+	this.sampler = MCMCSampler( this.probability );
+	this.sampler.burn_in();
+    }
+
     sampling (n, seed=null) {
 	/* n個の WaveFragment を取り出す */
 	/* seed が null でなければ乱数を初期化 */
-	/* from からの多次元正規分布で toを作る */
-	/* if this.probability(to)/this.probability(from) > rand() なら採用して from を変更 */
-	/*   if バーンイン中でなければ */
-	/*      採用したものは new WaveFragment( from.x, from.y, from.z, this.theta(), this.angular_velocity(), this) して結果に入れる */
-	/*      採用カウンタ ++ で十分ならreturn */
-	/* バーンインカウンタ ++ */
+	// Math.rand(seed);
+	samples = this.sampler.sample( 1000 );
+	return samples.map( s => WaveFragment(s.x .... ));
     }
     probability ( point_or_wavevalue ) {
 	/* this.waveValue を 取り出し */
-	/* 2乗して確立にして return */
+	/* 2乗して確率にして return */
     }
     theta( point_or_wavevalue ) {
 	/* this.waveValue を 取り出し */
@@ -25,10 +27,10 @@ export default class WaveFunction {
 	/* this.energy を 取り出し */
 	/* ごにょごにょ計算して return */
     }
-    energy( ) {
+    energy( point: {x:number, y:number, z:number} ): number {
 	error("need to 実装");
     }
-    waveValue( point ) {
+    waveValue( point: {x:number, y:number, z:number} ): {re: number, im: number} {
 	error("need to 実装");
     }
 }
