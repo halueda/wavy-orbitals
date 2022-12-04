7a3e4be29423ee79db59d94862351b38002b8840
Haruyasu Ueda <ueda.haruyasu@fujitsu-general.com>
2022-12-05 01:08:44 +0900

    WIP とりあえず書くだけ書いた

--- hydrogen.js
+++ hydrogen.js
@@ -12,18 +12,33 @@ function theta_fun(m, theta) {
 
 const phi_def = {0: {}, 1: {}, 2: {}, 3:{}};
 
-phi_def{0}{0} = (theta=> mathjs.chain(.....).....done());
+phi_def{0}{0}  = (theta=> mathjs.chain(1/2).sqrt().done());
+phi_def{1}{0}  = (theta=> mathjs.chain(3/2).sqrt().multiply(mathjs.cos(theta)).done());
+phi_def{1}{+1} = (theta=> mathjs.chain(3/4).sqrt().multiply(-mathjs.sin(theta)).done());
+phi_def{1}{-1} = (theta=> mathjs.chain(3/4).sqrt().multiply(+mathjs.sin(theta)).done());
+phi_def{2}{0}  = (theta=> mathjs.chain(5/8).sqrt().multiply(3*(mathjs.cos(theta))**2 -1).done());
+phi_def{2}{+1} = (theta=> mathjs.chain(15/4).sqrt().multiply(-mathjs.sin(theta)*mathjs.cos(theta)).done());
+phi_def{2}{-1} = (theta=> mathjs.chain(15/4).sqrt().multiply(+mathjs.sin(theta)*mathjs.cos(theta)).done());
+phi_def{2}{+2} = (theta=> mathjs.chain(15/16).sqrt().multiply(-mathjs.sin(theta)**2 -1).done());
+phi_def{2}{-2} = (theta=> mathjs.chain(15/16).sqrt().multiply(+mathjs.sin(theta)**2 -1).done());
 
 
 function phi_fun(l, m, theta) {
-  return phi_def{l}{m}(theta);
+  // if theta == NaN { theta = 2*PI*rand(); } // 不定 indefinite なので、適当な値にする
+  return phi_def[l][m](theta);
 }
 
 function Y(m, l, n, theta, phi) {
+  // if phi == NaN { phi = 2*PI*rand(); }  // 不定 indefinite なので、適当な値にする
   return theta_fun(m, theta).multiply( phi_fun(l, m, theta) );
 }
 
-function 極座標 (point) {
+function polar_coordinate (point) {
+  const {x, y, z} = point;
+  const r = mathjs.sqrt( x*x + y*y + z*z );
+  const theta = mathjs.acos( z/r );
+  const phi = mathjs.atan( y/x );
+  return {r: r, theta: theta, phi: phi};
   
 }
 
@@ -35,13 +50,13 @@ export default HydrogenOrbital extends WaveFunction {
     this.m = m;
   }
 
-  energy(_) {
-    return 1/n/n;
+  energy(p) {
+    return 1/ this.n / this.n;
   }
 
   
   waveValue(point) {
-    const (r, theta, phi) = 極座標(point);
+    const {r, theta, phi} = polar_coordinate(point);
     
     return Legendre(this.m, r) * Y(this.l, this.n, theta, phi);
   }
