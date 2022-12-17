63bc441ad721263f02e5ad374473dfc4599e143f
Haruyasu Ueda <ueda.haruyasu@fujitsu-general.com>
2022-12-17 21:57:34 +0900

    new: tr for stack-trace log. now function is loged in the object dump but not stringify.

--- util.js
+++ util.js
@@ -2,9 +2,31 @@
 
 export function ll(mes, result){
   console.log(mes);        
-  console.log(result.toString());
-  const stringed = JSON.stringify(result);
-  if (stringed != undefined) {
+//  console.log(result.toString());
+  const stringed = JSON.stringify(result, replacer);
+  if (stringed == undefined) {
+    console.log("undefined") ;
+  } else {
     console.log(JSON.parse(stringed)) ;
   }
+}
+
+export function tr(result){
+      try {
+        throw new Error("dummy");
+      } catch (e) {
+              const staray = e.stack.split( /\n/ );
+              staray.shift();
+              staray.shift();
+    //    ll( JSON.parse(JSON.stringify(staray)), result) ;
+        ll( {stack:{string:staray}}, result) ;
+      }
+}
+
+function replacer(k, v){
+        if (typeof k === "function") {
+                return v.toString();
+        } else {
+                return v;
+        }
 }
